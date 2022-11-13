dac95e569bcee165ae69bdca04951217a0a551fc
sahunitin023 <b521039@iiit-bh.ac.in>
2022-11-14 00:02:22 +0530

    first commit

---
+++ lib/screens/loading_screen.dart
@@ -0,0 +1,32 @@
+// ignore_for_file: library_private_types_in_public_api
+import 'package:geolocator/geolocator.dart';
+import 'package:flutter/material.dart';
+
+class LoadingScreen extends StatefulWidget {
+  const LoadingScreen({Key? key}) : super(key: key);
+
+  @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  void getPosition() async {
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.low);
+    print(position);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            getPosition();
+          },
+          child: const Text('Get Location'),
+        ),
+      ),
+    );
+  }
+}
