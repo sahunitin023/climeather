00356e6a75ebb1bc692c463f39ea9f2fd65a8070
sahunitin023 <b521039@iiit-bh.ac.in>
2023-12-23 12:59:14 +0530

    Updated UI

--- android/app/src/main/kotlin/com/example/clima_app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.clima_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ lib/data/my_data.dart
@@ -0,0 +1,3 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+String API_KEY = dotenv.env['OPEN_WEATHER_API_KEY'].toString();
